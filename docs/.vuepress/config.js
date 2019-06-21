09797bd44499a9184ccdd61e1a92d6eedd1ca714
maki maki <chenjunhong54321@163.com>
2019-06-21 15:55:04 +0800

    啊

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
         lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
+        sidebar: 'auto',
         nav: [
             { text: '前端', link: '/' },
             { text: '算法', link: '/guide/' },
@@ -36,29 +37,18 @@ module.exports = {
             { text: 'Github', link: 'https://github.com/cjhyy' },
         ],
         sidebar: [{
-            // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
-            '/leetcode/': [
-                '/leetcode/', // accumulate文件夹的README.md 不是下拉框形式
-                {
-                    title: '侧边栏下拉框的标题1',
-                    children: [
-                        '/leetcode/LeetCode 解题报告 1', // 以docs为根目录来查找文件 
-                        // 上面地址查找的是：docs>accumulate>JS>test.md 文件
-                        // 自动加.md 每个子选项的标题 是该md文件中的第一个h1/h2/h3标题
-                    ]
-                }
-            ],
-            // docs文件夹下面的algorithm文件夹 这是第二组侧边栏 跟第一组侧边栏没关系
-            '/algorithm/': [
-                '/algorithm/',
-                {
-                    title: '第二组侧边栏下拉框的标题1',
-                    children: [
-                        '/algorithm/simple/test'
-                    ]
-                }
-            ]
-        }]
+                title: '入门',
+                collapsable: false,
+                children: [
+                    '/leetcode/',
+                    'get-started/',
+                ]
+            },
+            {
+                title: '组件',
+                children: ['/components/button']
+            }
+        ]
 
     }
 }
