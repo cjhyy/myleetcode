c5810884ac075b5f1db7fea7f4d1a79c35b08696
maki maki <chenjunhong54321@163.com>
2019-06-22 15:18:40 +0800

    啊

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,13 +2,16 @@ module.exports = {
     title: 'LeetCode题解',
     description: '记录我的LeetCode题解',
     head: [
-        ['link', { rel: 'icon', href: '/logo.png' }], // 增加一个自定义的 favicon(网页标签的图标)
+        ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     markdown: {
         lineNumbers: true // 代码块显示行号
     },
     themeConfig: {
         sidebar: 'auto',
+        lastUpdated: '最后更新时间',
+        // 2.设置true，开启最后更新时间
+        lastUpdated: true,        
         nav: [
             { text: '前端', link: 'https://github.com/OBKoro1' },
             {
@@ -18,6 +21,7 @@ module.exports = {
                     { text: 'leetcode题解', link: '/' }
                 ]
             },
+     
             {
                 text: '学习笔记',
                 items: [
@@ -48,6 +52,9 @@ module.exports = {
                 children: [
                     '/leetcode/1',
                     '/leetcode/2',
+                    '/leetcode/3',
+                    '/leetcode/4',
+                    '/leetcode/7',
                 ]
             },
             {
